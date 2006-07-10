a4010729f4fca92887768d3159cd70b3dc08ef0d
Edgar Gabriel <egabriel@central.uh.edu>
2006-07-10 20:42:48 +0000

    fix the name of the variables used for reading

--- src/framework/ADCL_readenv.c
+++ src/framework/ADCL_readenv.c
@@ -5,8 +5,8 @@
 #define PATH_MAX 40
 #define MAXLINE 40
 
-extern int ADCL_OUTLITER_FACTOR;
-extern int ADCL_OUTLITER_FRACTION;
+extern int ADCL_OUTLIER_FACTOR;
+extern int ADCL_OUTLIER_FRACTION;
 
 int ADCL_read_env()
 {
@@ -14,9 +14,6 @@ int ADCL_read_env()
     int len;
     int i;
     int flag;
-
-    int ADCL_OUTLITER_FACTOR;
-    int ADCL_OUTLITER_FRACTION;
 
     char *HomeDir;
     char FilePath[PATH_MAX];
@@ -30,82 +27,74 @@ int ADCL_read_env()
     HomeDir = getenv("HOME");
 
     if (HomeDir == NULL) {
-	   ADCL_printf("Environment variable HOME not found\n");
-	   exit(1);
+	ADCL_printf("Environment variable HOME not found\n");
+	return ADCL_NOT_FOUND;
     }
     result = snprintf(FilePath, PATH_MAX, "%s/.adcl/config.adcl", HomeDir);
-
-	if (result >= PATH_MAX) {
-	   ADCL_printf("Warning: path \"%s\" too long. Truncated.\n", FilePath);
+    
+    if (result >= PATH_MAX) {
+	ADCL_printf("Warning: path \"%s\" too long. Truncated.\n", FilePath);
     }
 
     fp = fopen(FilePath, "r");
-
-	if (fp == NULL) {
-	   ADCL_printf("Could not open %s\n", FilePath);
-	   exit(1);
+    if (fp == NULL) {
+	ADCL_printf("Could not open %s\n", FilePath);
+	return ADCL_NOT_FOUND;
     }
 
 #ifdef Debug
     ADCL_printf("Outputting contents of %s:\n\n", FilePath);
 #endif 
 
-	/*Read lines from configure file*/
-	while (fgets(buffer, MAXLINE, fp) != NULL) {
-
-		/* check blank line or # comment */
-	    if( buffer[0] != '#' ){
-		
-			/* Parse one single line! */
-			i = 0;
-			flag =0;
-			len = strlen(buffer);
-
-			while(i<len) {
-		    	temp[i]= buffer[i];
-		    	if(buffer[i] == ':'){
-					strncpy(keyword, temp, i);
+    /*Read lines from configure file*/
+    while (fgets(buffer, MAXLINE, fp) != NULL) {
+	
+	/* check blank line or # comment */
+	if( buffer[0] != '#' ){
+	    /* Parse one single line! */
+	    i = 0;
+	    flag =0;
+	    len = strlen(buffer);
+	    
+	    while(i<len) {
+		temp[i]= buffer[i];
+		if(buffer[i] == ':'){
+		    strncpy(keyword, temp, i);
 #ifdef Debug
-					ADCL_printf("Keyword is %s\n",keyword);
+		    ADCL_printf("Keyword is %s\n",keyword);
 #endif
-					i++;
-					flag =1;	/* this means this line has keywords; */
-					ptr = &buffer[i];
-					break;	
-		    }
-		    /* 	else{ */
-		    /* 		printf("This is a NULL line!\n"); */
-		    /* 	} */
 		    i++;
-			}
-			
-			/*  If this is a keyworkd...... */
-			if(flag ==1){
-				/*  ADCL_OUTLIER_FACTOR; */
-				if(strncmp(keyword, "ADCL_OUTLIER_FACTOR", strlen("ADCL_OUTLIER_FACTOR")) == 0){
-				
-				sscanf(ptr, "%d", &ADCL_OUTLITER_FACTOR);
-#ifndef	Debug	
-				ADCL_printf("ADCL_OUTLITER_FACTOR : %d\n", ADCL_OUTLITER_FACTOR);	
-#endif
-				ptr = NULL;
-	
-				}
-	
-				if(strncmp(keyword, "ADCL_OUTLIER_FRACTION", strlen("ADCL_OUTLIER_FRACTION")) == 0){
-				
-				sscanf(ptr, "%d", &ADCL_OUTLITER_FRACTION);
-#ifndef	Debug	
-				ADCL_printf("ADCL_OUTLITER_FRACTION : %d\n", ADCL_OUTLITER_FRACTION);	
-#endif
-				ptr = NULL;
-	
-				}
-				
-	
-			}
+		    flag =1;	/* this means this line has keywords; */
+		    ptr = &buffer[i];
+		    break;	
 		}
+		/* 	else{ */
+		/* 		printf("This is a NULL line!\n"); */
+		/* 	} */
+		i++;
+	    }
+	    
+	    /*  If this is a keyworkd...... */
+	    if(flag == 1){
+		/*  ADCL_OUTLIER_FACTOR; */
+		if(strncmp(keyword, "ADCL_OUTLIER_FACTOR", strlen("ADCL_OUTLIER_FACTOR")) == 0){
+		    sscanf(ptr, "%d", &ADCL_OUTLIER_FACTOR);
+#ifndef	Debug	
+		    ADCL_printf("ADCL_OUTLIER_FACTOR : %d\n", ADCL_OUTLIER_FACTOR);	
+#endif
+		    ptr = NULL;
+		}
+		
+		if(strncmp(keyword, "ADCL_OUTLIER_FRACTION", strlen("ADCL_OUTLIER_FRACTION"))==0){
+		    sscanf(ptr, "%d", &ADCL_OUTLIER_FRACTION);
+#ifndef	Debug	
+		    ADCL_printf("ADCL_OUTLIER_FRACTION : %d\n", ADCL_OUTLIER_FRACTION);	
+#endif
+		    ptr = NULL;
+		}
+	    }
 	}
-
-	return ADCL_SUCCESS;
+    }
+    
+    return ADCL_SUCCESS;
 }
