d0989af9a9495d58aaf27868a1b6a63ab9e24e57
Edgar Gabriel <egabriel@central.uh.edu>
2007-03-14 20:54:40 +0000

    first cut on the test for user-registered functions

---
+++ test/test-fnctset-noattrs.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+#include "ADCL.h"
+#include "mpi.h"
+
+#define NIT 500
+
+void test_func_1 ( ADCL_Request req, int *a, int* b, int *c );
+void test_func_2 ( ADCL_Request req, int *a, int* b, int *c );
+void test_func_3 ( ADCL_Request req, int *a, int* b, int *c );
+
+
+
+int main ( int argc, char ** argv ) 
+{
+    int i, rank, size;
+
+    ADCL_Function funcs[3];
+    ADCL_Fnctset fnctset;
+    ADCL_Request request;
+    
+    MPI_Init ( &argc, &argv );
+    MPI_Comm_rank ( MPI_COMM_WORLD, &rank );
+    MPI_Comm_size ( MPI_COMM_WORLD, &size );
+
+    ADCL_Init ();
+
+    ADCL_Function_create ( (ADCL_work_fnct_ptr *)test_func_1, ADCL_ATTRSET_NULL, NULL, 
+			   "test_func_1", &(funcs[0]));
+
+    ADCL_Function_create ( (ADCL_work_fnct_ptr *)test_func_2, ADCL_ATTRSET_NULL, NULL, 
+			   "test_func_2", &(funcs[1]));
+
+    ADCL_Function_create ( (ADCL_work_fnct_ptr *)test_func_3, ADCL_ATTRSET_NULL, NULL, 
+			   "test_func_3", &(funcs[2]));
+
+    ADCL_Fnctset_create ( 3, funcs, "trivial functions", &fnctset );
+    ADCL_Request_create_fnctset ( fnctset, &request );
+    
+
+    for ( i=0; i<NIT; i++ ) {
+	ADCL_Request_start( request );
+    }
+
+    ADCL_Request_free ( &request );
+    ADCL_Fnctset_free ( &fnctset );
+    
+    for ( i=0; i<3; i++ ) {
+	ADCL_Function_free ( &funcs[0] );
+    }
+
+    
+    ADCL_Finalize ();
+    MPI_Finalize ();
+    return 0;
+}
+
+void test_func_1 ( ADCL_Request req, int *a, int* b, int *c )
+{
+    MPI_Comm comm;
+    int rank;
+
+    ADCL_Request_get_comm ( req, &comm );
+    ADCL_Request_get_rank ( req, &rank );
+    printf("%d: In test_func_1, a=%d, b=%d, c=%d \n", rank, *a, *b, *c );
+    return;
+}
+
+void test_func_2 ( ADCL_Request req, int *a, int* b, int *c )
+{
+    MPI_Comm comm;
+    int rank;
+
+    ADCL_Request_get_comm ( req, &comm );
+    ADCL_Request_get_rank ( req, &rank );
+    printf("%d: In test_func_2, a=%d, b=%d, c=%d \n", rank, *a, *b, *c );
+    return;
+}
+
+void test_func_3 ( ADCL_Request req, int *a, int* b, int *c )
+{
+    MPI_Comm comm;
+    int rank;
+
+    ADCL_Request_get_comm ( req, &comm );
+    ADCL_Request_get_rank ( req, &rank );
+    printf("%d: In test_func_3, a=%d, b=%d, c=%d \n", rank, *a, *b, *c );
+    return;
+}
