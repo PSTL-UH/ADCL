6863363a2045f827029e313aa1becdbc845e105b
mchaarawi <mchaarawi@gmail.com>
2007-03-20 17:15:05 +0000

    added checks for the papi structure

--- src/adcl/C/ADCL_Papi.c
+++ src/adcl/C/ADCL_Papi.c
@@ -3,25 +3,40 @@
 
 int ADCL_Papi_create ( ADCL_Papi *papi )
 {
+    if ( NULL == papi ) {
+	return ADCL_INVALID_PAPI;
+    }
     return ADCL_papi_create (papi);
 }
 
 int ADCL_Papi_enter ( ADCL_Papi papi ) 
 {
+    if ( NULL == papi ) {
+	return ADCL_INVALID_PAPI;
+    }
     return ADCL_papi_enter (papi);
 }
 
 int ADCL_Papi_leave ( ADCL_Papi papi ) 
 {
+    if ( NULL == papi ) {
+	return ADCL_INVALID_PAPI;
+    }
     return ADCL_papi_leave (papi);
 }
 
 int ADCL_Papi_free ( ADCL_Papi *papi)
 {
+    if ( NULL == papi ) {
+	return ADCL_INVALID_PAPI;
+    }
     return ADCL_papi_free (papi);
 }
 
 int ADCL_Papi_print ( ADCL_Papi papi)
 {
+    if ( NULL == papi ) {
+	return ADCL_INVALID_PAPI;
+    }
     return ADCL_papi_print (papi);
 }
