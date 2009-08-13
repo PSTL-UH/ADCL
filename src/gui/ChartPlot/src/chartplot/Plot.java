954bc38e21af64d1aa4224fdf9a19f32cdd1f86e
sapoluri <sarat.poluri@gmail.com>
2009-08-13 18:04:11 +0000

    A    gui/ChartPlot A    gui/ChartPlot/src A    gui/ChartPlot/src/chartplot A    gui/ChartPlot/src/chartplot/Plot.java A    gui/ChartPlot/src/communicationmanager A    gui/ChartPlot/src/communicationmanager/StartupConnection.java A    gui/ChartPlot/src/communicationmanager/ConfigurationModel.java A    gui/ChartPlot/src/communicationmanager/Header.java A    gui/ChartPlot/src/communicationmanager/StartupServer.java A    gui/ChartPlot/src/utility A    gui/ChartPlot/src/utility/Utility.java A    gui/ChartPlot/src/Main A    gui/ChartPlot/src/Main/ChartPlotController.java A    gui/ChartPlot/src/Main/Main.java A    gui/ChartPlot/src/icons AM   gui/ChartPlot/src/icons/print.gif A    gui/ChartPlot/src/icons/AUTHOR AM   gui/ChartPlot/src/icons/fit-width.gif AM   gui/ChartPlot/src/icons/export.gif A    gui/ChartPlot/src/eventpackage A    gui/ChartPlot/src/eventpackage/RecvEvent.java A    gui/ChartPlot/src/eventpackage/RecvListener.java A    gui/ChartPlot/src/eventpackage/EventGenerator.java A    gui/ChartPlot/src/plot A    gui/ChartPlot/src/plot/InteractiveGraph.java A    gui/ChartPlot/src/plot/RenderUtils.java A    gui/ChartPlot/src/plot/Graph.java A    gui/ChartPlot/src/plot/Axis.java A    gui/ChartPlot/src/plot/LinearTimeIndexScheme.java A    gui/ChartPlot/src/plot/LinearDecimalIndexScheme.java A    gui/ChartPlot/src/plot/FunctionInstance.java A    gui/ChartPlot/src/plot/TerminatorFactory.java A    gui/ChartPlot/src/plot/LowerLimitInstance.java A    gui/ChartPlot/src/plot/YAxisInstance.java A    gui/ChartPlot/src/plot/package-info.java A    gui/ChartPlot/src/plot/GraphInstance.java A    gui/ChartPlot/src/plot/LinearIndexScheme.java A    gui/ChartPlot/src/plot/ExportListener.java A    gui/ChartPlot/src/plot/GraphDomain.java A    gui/ChartPlot/src/plot/AxisInstance.java A    gui/ChartPlot/src/plot/ZoomSelection.java A    gui/ChartPlot/src/plot/IndexScheme.java A    gui/ChartPlot/src/plot/XAxis.java A    gui/ChartPlot/src/plot/LineChartInstance.java A    gui/ChartPlot/src/plot/ToolBar.java A    gui/ChartPlot/src/plot/ChartType.java A    gui/ChartPlot/src/plot/ToolBarFader.java A    gui/ChartPlot/src/plot/UpperLimitInstance.java A    gui/ChartPlot/src/plot/XAxisInstance.java A    gui/ChartPlot/src/plot/ChartStyle.java A    gui/ChartPlot/src/plot/StepChartInstance.java A    gui/ChartPlot/src/plot/FileNameExtensionFilter.java A    gui/ChartPlot/src/plot/Function.java A    gui/ChartPlot/src/plot/CoordinateBoundary.java A    gui/ChartPlot/src/plot/YAxis.java

---
+++ src/gui/ChartPlot/src/chartplot/Plot.java
@@ -0,0 +1,169 @@
+
+package chartplot;
+
+/**
+ *
+ * @author sarat
+ */
+
+import java.awt.*;
+import java.util.*;
+import java.math.*;
+import javax.swing.*;
+
+import Main.ChartPlotController;
+
+import communicationmanager.Header;
+import communicationmanager.StartupServer;
+
+import eventpackage.*;
+import plot.*;
+
+@SuppressWarnings("serial")
+public final class Plot extends JFrame implements RecvListener
+{
+	
+	private int noOfGraphs = 0;
+    XAxis xAxis = new XAxis("No. of Iterations");
+    YAxis yAxis = new YAxis("Time Elapsed");// make changes in axisinstance in the paint function
+    private final ArrayList<Graph> graph = new ArrayList<Graph>();
+    HashMap<Integer, Graph> idToTabMap;
+    HashMap<Integer, Integer> idToIteration;
+    JTabbedPane tab = null;
+    ChartPlotController _controller;
+
+	public Plot(StartupServer server) 
+	{
+		ChartPlotController controller = new ChartPlotController(this,server);
+		_controller = controller;
+		idToTabMap = new HashMap<Integer, Graph>();
+		idToIteration = new HashMap<Integer, Integer>();
+		initGraph();
+		initFunctions();
+		initComponent();			
+	}
+
+    private void initComponent()
+    {
+    	
+        tab = new JTabbedPane();
+        getContentPane().add(tab);
+        
+        for(int i=0;i<graph.size();i++)
+        {
+        	addComponentsToTab(i);
+        }
+    }
+
+	private void addComponentsToTab(int i) 
+	{
+		JButton disconnect = new JButton("Disconnect");
+		disconnect.setBounds(400,10,100,25);
+		disconnect.addActionListener(_controller);
+		Graph graphToAdd = idToTabMap.get(Integer.valueOf(i));
+		graphToAdd.add(disconnect,BorderLayout.AFTER_LAST_LINE);
+
+		Panel textPanel = new Panel();
+		textPanel.setBounds(0,520, 640, 100);
+		TextArea messageBox = new TextArea("", 5, 80, 1);
+		
+		textPanel.add(messageBox);
+		graphToAdd.add(textPanel,BorderLayout.AFTER_LAST_LINE);		
+
+		tab.addTab("Tab "+i, graphToAdd);
+	}
+
+
+    private void initGraph() 
+    {    
+    	for(int i=0;i<noOfGraphs ;i++)
+    	{
+    		addNewGraphToList();
+    	}
+	}
+
+	private void addNewGraphToList() 
+	{
+		Insets padding = new Insets(50, 50, 150, 50);
+		Graph newGraph = new InteractiveGraph(xAxis, yAxis);    		
+		newGraph.getXAxis().setZigZaginess(BigDecimal.valueOf(7L, 1));
+		newGraph.getYAxis().setZigZaginess(BigDecimal.valueOf(7L, 1));
+		newGraph.setBackground(Color.WHITE);
+		newGraph.setPadding(padding);
+		graph.add(newGraph);
+	}
+
+
+    private void initFunctions()
+    {
+		for(int i=0;i<graph.size();i++)
+		{
+			addFunctionToGraph(i);
+		}
+    }
+
+	private void addFunctionToGraph(int i) {
+		ChartStyle style2 = new ChartStyle();
+		style2.setPaint(Color.GREEN);
+		idToTabMap.get(Integer.valueOf(i)).addFunction(new Function("Function "+i), style2);
+	}
+
+    public void addPointtoGraph(int index,double x,double y)
+    {
+    	BigDecimal xPoint = new BigDecimal(x);
+    	BigDecimal yPoint = new BigDecimal(y);
+    	Graph updateGraph = idToTabMap.get(Integer.valueOf(index));
+    	Object[] functions = updateGraph.getGraphFunctions();
+    	for(Object func : functions)
+    	{
+    		Function graphFunc = (Function)func;
+    		graphFunc.addPoint(xPoint, yPoint);
+    	}
+    }
+
+    
+    @Override
+    public void recvReceived(RecvEvent event) 
+    {
+		byte[] msg = event.getRecvData();
+		Header head = event.get_header();
+				
+		if(!idToTabMap.containsKey(Integer.valueOf(head.get_id())))
+		{
+			addNewGraphToList();
+			idToTabMap.put(Integer.valueOf(head.get_id()), graph.get(graph.size() - 1));
+			idToIteration.put(Integer.valueOf(head.get_id()), Integer.valueOf(1));
+			addFunctionToGraph(head.get_id());
+			addComponentsToTab(head.get_id());					
+		}
+		if( head.get_type() == 0)
+		{    	
+			Integer iteration = idToIteration.get(head.get_id());
+			addPointtoGraph(head.get_id(), iteration.doubleValue(), head.get_yval());
+			idToIteration.remove(head.get_id());
+			iteration = Integer.valueOf(iteration.intValue() + 1);	
+			idToIteration.put(head.get_id(), iteration);
+		}
+		else if(head.get_type() == 1)
+		{
+			showInMessageBox(head.get_id(),new String(msg));
+		}
+		
+		for(Graph eachGraph : graph)
+		{
+			eachGraph.render();
+			eachGraph.repaint();
+		}
+    	
+    }
+
+	private void showInMessageBox(int id, String str) 
+	{
+		System.out.println(str);
+			Panel panel = (Panel)idToTabMap.get(id).getComponent(2);
+			TextArea textbox = (TextArea)panel.getComponent(0);
+			textbox.append(str+"\n");
+	}
+}
+
+
