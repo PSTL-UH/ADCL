dab8dbf2234206b5eb7cc552e3cf12ed5e682c21
sapoluri <sarat.poluri@gmail.com>
2009-09-14 16:42:09 +0000

    Restart Functionality and Set Y added

--- src/gui/ChartPlot/src/Main/ChartPlotController.java
+++ src/gui/ChartPlot/src/Main/ChartPlotController.java
@@ -1,27 +1,73 @@
 package Main;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import chartplot.Plot;
 
 import communicationmanager.StartupServer;
 
-public class ChartPlotController implements ActionListener
+public class ChartPlotController implements MouseListener
 {
 	StartupServer _server;
 	Plot _plot;
+	Dimension _windowSize;
+	boolean restart = false;
 	
-	public ChartPlotController(Plot plot, StartupServer server) 
+	public boolean isRestart() {
+		return restart;
+	}
+
+	public void setRestart(boolean restart) {
+		this.restart = restart;
+	}
+
+	public ChartPlotController(Plot plot, StartupServer server, Dimension windowSize) 
 	{
 		_plot = plot;
 		_server = server;
+		_windowSize = windowSize;
+		
+	}	
+	
+	@Override
+	public void mouseClicked(MouseEvent event)
+	{		
+		if(event.getComponent().getName() == "Disconnect")
+		{
+			_server.disconnect();
+			System.out.println("disconnect");
+		}
+		else if(event.getComponent().getName() == "Start Over")
+		{
+			System.out.println("Start over");
+		    restart = true;
+		}
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) 
+	public void mouseEntered(MouseEvent arg0) 
+	{		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) 
 	{
-		_server.disconnect();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) 
+	{
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) 
+	{		
 	}
 
 }
